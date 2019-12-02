69a8a591b54609022b2bfcac684f7a4b81f0e986
Thomas Herzog <Tomrule007@gmail.com>
2019-12-02 14:48:25 -0800

    Day 1 - part 2 Complete

--- js/day 1/index.js
+++ js/day 1/index.js
@@ -1,7 +1,24 @@
 import inputData from './inputData.js';
 console.log({ inputData });
-const moduleMassToFuel = mass => Math.floor(mass / 3) - 2;
 const sum = (a, b) => a + b;
+const massToFuel = mass => {
+  const fuel = Math.floor(mass / 3) - 2;
+  return fuel <= 0 ? 0 : fuel;
+};
 
-const totalSpacecraftFuel = inputData.map(moduleMassToFuel).reduce(sum);
+const totalSpacecraftFuel = inputData.map(massToFuel).reduce(sum);
 console.log({ totalSpacecraftFuel });
+
+// Part 2
+
+export const massToFuelRecursive = fuelMass => {
+  const mass = massToFuel(fuelMass);
+  console.log({ mass });
+  if (mass === 0) return 0;
+  return mass + massToFuelRecursive(mass);
+};
+
+const totalSpacecraftFuelIncludingFuel = inputData
+  .map(massToFuelRecursive)
+  .reduce(sum);
+console.log({ totalSpacecraftFuelIncludingFuel });
